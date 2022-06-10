c8320748ac15e983e09fcbafdb912344bf71a197
sharanvkt <sharanvkt@gmail.com>
2022-06-11 02:18:06 +0530

    till myNFT page

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect, useState, useContext } from "react";
+import Image from "next/image";
+
+import { NFTContext } from "../context/NFTContext";
+import { Loader, NFTCard, Button } from "../components";
+import images from "../assets";
+import { shortenAddress } from "../utils/shortenAddress";
+
 
 const NFTDetails = () => {
+  
   return (
     <div>NFTDetails</div>
   )
